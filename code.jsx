8b4005d967c1754c75d643ad4816e92e7e697f0a
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-08 21:50:17 -0500

    Sneaky new changes

--- code.jsx
+++ code.jsx
@@ -1,12 +1,9 @@
-
 import React from 'react';
 
-function App() {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-        </div>
-    );
-}
+const TheAPP = () => (
+    <div>
+        <h1>Hello, world!</h1>
+    </div>
+);
 
-export default App;
+export default TheAPP;
