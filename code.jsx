c1810ce373ea2fc57aca4958a17f1061ff1940c7
Spencer Fasulo <spencer.fasulo@icloud.com>
2023-11-08 21:48:12 -0500

    today is great

--- code.jsx
+++ code.jsx
@@ -1,10 +1,10 @@
-
 import React from 'react';
 
 function App() {
     return (
         <div>
             <h1>Hello, world!</h1>
+            <p>Today is a great day</p>
         </div>
     );
 }
